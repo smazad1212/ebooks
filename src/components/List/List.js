839da6eb81504b29b1a05c0a97775c83b305d17a
S M Azad Rahman <smazad1212@gmail.com>
2019-09-29 18:26:44 +0600

    get request added for list

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,78 +1,85 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Table, Button } from 'antd'
+import axios from 'axios'
 
 import list from './List.module.css'
 
-const List = (
-  {
-    history
-  }) => {
-  const columns = [
-    {
-      title: 'Title',
-      dataIndex: 'title',
-      render: (title, record) => (
-        <span>
-          <img src={record.cover} alt={title} className={list.bookCover} />
+class List extends Component {
+  state = {
+    loading: true,
+    books: [],
+    error: false
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000/api/books').then(res => {
+      let data = res.data
+      this.setState({
+        books: data,
+        loading: false,
+        error: false
+      })
+    })
+      .catch(error => {
+        this.setState({
+          loading: false,
+          error: true
+        })
+      })
+  }
+
+  render() {
+    const { loading, books, error } = this.state
+
+    const columns = [
+      {
+        title: 'Title',
+        dataIndex: 'title',
+        render: (title, record) => (
+          <span>
+          <img src={record.cover} alt='' className={list.bookCover} />
           <span>{title}</span>
         </span>
-      ),
-    },
-    {
-      title: 'Author',
-      dataIndex: 'author',
-    },
-    {
-      title: 'Genre',
-      dataIndex: 'genre',
-    },
-    {
-      title: 'Download',
-      render: link => (
-        <a href='/'>Download</a>
-      ),
-    },
-  ];
+        ),
+      },
+      {
+        title: 'Author',
+        dataIndex: 'author',
+      },
+      {
+        title: 'Genre',
+        dataIndex: 'genre',
+      },
+      {
+        title: 'Download',
+        render: link => (
+          <a href='/'>Download</a>
+        ),
+      },
+    ]
 
-  const data = [
-    {
-      key: '1',
-      cover: 'https://picsum.photos/200',
-      title: 'Warriors And Cats',
-      author: 'John Brown',
-      genre: 'Mystery',
-      downloadLink: 'https://picsum.photos/200'
-    },
-    {
-      key: '2',
-      cover: 'https://picsum.photos/200/300',
-      title: 'Bow Of The Forest',
-      author: 'Jim Green',
-      genre: 'Horror',
-      downloadLink: 'https://picsum.photos/200'
-    },
-    {
-      key: '3',
-      cover: 'https://picsum.photos/200',
-      title: 'Running In The Depths',
-      author: 'Joe Black',
-      genre: 'Romance',
-      downloadLink: 'https://picsum.photos/200'
-    },
-  ];
+    if (loading) {
+      return <div>Loading . . .</div>
+    }
 
-  return (
-    <div className={list.container}>
-      <Table
-        className={list.table}
-        columns={columns}
-        dataSource={data}
-        bordered
-        pagination={false}
-      />
-      <Button type="primary" icon='plus' onClick={() => history.push('/add')}>Add New</Button>
-    </div>
-  );
-};
+    if (error) {
+      return <div>Something went wrong!</div>
+    }
 
-export default List;
+    return (
+      <div className={list.container}>
+        <Table
+          className={list.table}
+          columns={columns}
+          dataSource={books}
+          rowKey='_id'
+          bordered
+          pagination={false}
+        />
+        <Button type="primary" icon='plus' onClick={() => this.props.history.push('/add')}>Add New</Button>
+      </div>
+    )
+  }
+}
+
+export default List
